5ba90cd7cbff40bbe6a11cbce7b9324c949020fa
TheWinterDolphin <andrew.sorak@gmail.com>
2022-10-19 12:36:29 -0500

    started linkedList

--- src/CircularLinkedList.java
+++ src/CircularLinkedList.java
@@ -1,2 +1,11 @@
 public class CircularLinkedList<T> {
+    private Link<T> first;
+
+    public CircularLinkedList() {
+        first = null;
+    }
+
+    public void insertFirst(T newData) {
+        
+    }
 }
--- src/Link.java
+++ src/Link.java
@@ -1,3 +1,12 @@
 public class Link<T> {
-    
+    public T data;
+    public Link next;
+
+    public Link(T newData) {
+        data = newData;
+    }
+
+    public void replaceData(T newData) {
+        data = newData;
+    }
 }
