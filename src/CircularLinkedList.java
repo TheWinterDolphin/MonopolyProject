cca96bdde7e546e046da9b7b0180d623c2586be0
TheWinterDolphin <andrew.sorak@gmail.com>
2022-10-19 12:46:43 -0500

    Circular Linked List Construction and Insertion DONE

--- src/CircularLinkedList.java
+++ src/CircularLinkedList.java
@@ -6,6 +6,27 @@ public class CircularLinkedList<T> {
     }
 
     public void insertFirst(T newData) {
-        
+        Link<T> newLink = new Link<T>(newData);
+        if (first == null) {
+            newLink.next = newLink;
+            first = newLink;
+        }
+        else {
+            newLink.next = first;
+            Link<T> current = first;
+            while(current.next != first) {
+                current = current.next;
+            }
+            current.next = newLink;
+            first = newLink;
+        }
+    }
+
+    public void devPrint(int numOfLinks) {
+        Link<T> current = first;
+        for (int i=0; i<numOfLinks; i++) {
+            System.out.print("" + current.data + " --> ");
+            current = current.next;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.print("Hi!");
+        CircularLinkedList<Integer> myLinkedList = new CircularLinkedList<Integer>();
+        myLinkedList.insertFirst(10);
+        myLinkedList.insertFirst(5);
+        myLinkedList.insertFirst(3);
+        myLinkedList.devPrint(10);
     }
 }
