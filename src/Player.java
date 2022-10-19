acac68cdf2e70cdb33e7b35c2db5fee7acf7e702
jayathekdi <jayathekdi@gmail.com>
2022-10-19 12:39:16 -0500

    Updated Player class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Player {
+    private String name;
+    private Link<BoardSpace> location;
+    private int money;
+    private int turnsLeftInJail;
+    private ArrayList<BoardSpace> properties;
+
+    public Player(String name) {
+        this.name = name;
+        BoardSpace go = new BoardSpace("GO");
+        location = new Link<BoardSpace>(go);
+        money = 1500;
+        properties = null;
+    }
 }
