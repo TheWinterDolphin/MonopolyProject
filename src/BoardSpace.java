301b8636b6d08668401ed6c41d1a49439cb7706c
jayathekdi <jayathekdi@gmail.com>
2022-10-19 12:46:34 -0500

    Updated BoardSpace class

--- src/BoardSpace.java
+++ src/BoardSpace.java
@@ -1,2 +1,6 @@
 public class BoardSpace {
+    private String type; // options: Property, GO, Chance, Community Chest, Utility, Railroad, To Jail, Tax, Blank
+    public BoardSpace(String type) {
+        this.type = type;
+    }
 }
