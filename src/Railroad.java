18be773dbba562f8552ea18e043c779657991f88
jayathekdi <jayathekdi@gmail.com>
2022-10-20 08:46:01 -0500

    Updated Property, Utility, Railroad, and TaxSpace classes

--- src/Railroad.java
+++ src/Railroad.java
@@ -1,2 +1,13 @@
 public class Railroad extends BoardSpace {
+    private Player owner;
+    private int rent;
+    private String name;
+    private int numOwnedBySamePlayer;
+
+    public Railroad(String name, int rent) {
+        super("Railroad");
+        this.name = name;
+        this.rent = rent;
+        numOwnedBySamePlayer = 0;
+    }
 }
--- src/Utility.java
+++ src/Utility.java
@@ -1,2 +1,11 @@
 public class Utility extends BoardSpace {
+    private Player owner;
+    private boolean bothOwned; // rent is dice roll * 4 if 1 owned, * 10 if both owned
+    private int price;
+
+    public Utility(int price) {
+        super("Utility");
+        this.price = price;
+    }
+
 }
