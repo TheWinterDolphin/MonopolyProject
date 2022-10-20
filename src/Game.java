54e49a5497d425f3e6a38f48dda3a14c6d31a8a7
TheWinterDolphin <andrew.sorak@gmail.com>
2022-10-20 12:05:48 -0500

    printBoard works now

--- src/Game.java
+++ src/Game.java
@@ -2,30 +2,102 @@ public class Game {
     private CircularLinkedList<BoardSpace> spaces;
     private CircularLinkedList<Player> playerTurnOrder;
 
-    private void printBoard() {
-        System.out.println("");
+    public void printBoard() {
         System.out.println("--------------------------------------------------------------------------------");
 
         //TopRow
         String topRowString = "| ";
+        topRowString += colorSpaceString("JAIL", "[JAIL]");
+        topRowString += " ";
+        topRowString += colorSpaceString("C1", "[ C1 ]");
+        topRowString += " ";
+        topRowString += colorSpaceString("EC", "[ EC ]");
+        topRowString += " ";
+        topRowString += colorSpaceString("C2", "[ C2 ]");
+        topRowString += " ";
+        topRowString += colorSpaceString("C3", "[ C3 ]");
+        topRowString += " ";
+        topRowString += colorSpaceString("R2", "[ R2 ]");
+        topRowString += " ";
+        topRowString += colorSpaceString("D1", "[ D1 ]");
+        topRowString += " ";
+        topRowString += colorSpaceString("CC_TopRow", "[ CC ]");
+        topRowString += " ";
+        topRowString += colorSpaceString("D2", "[ D2 ]");
+        topRowString += " ";
+        topRowString += colorSpaceString("D3", "[ D3 ]");
+        topRowString += " ";
+        topRowString += colorSpaceString("FrPk", "[FrPk]");
+        topRowString += " |";
+        System.out.println(topRowString);
 
-        /*
-                | [JAIL] [ C1 ] [ EC ] [ C2 ] [ C3 ] [ R2 ] [ D1 ] [ CC ] [ D2 ] [ D3 ] [FrPk] |
-                | [ B3 ] -------------------------------------------------------------- [ E1 ] |
-                | [ B2 ] |                                                            | [ CH ] |
-                | [ CH ] |                                                            | [ E2 ] |
-                | [ B1 ] |                                                            | [ E3 ] |
-                | [ R1 ] |                      M O N O P O L Y                       | [ R3 ] |
-                | [ IT ] |                                                            | [ F1 ] |
-                | [ A2 ] |                                                            | [ F2 ] |
-                | [ CC ] |                                                            | [ WW ] |
-                | [ A1 ] -------------------------------------------------------------- [ F3 ] |
-                | [ GO ] [ H2 ] [ LT ] [ H1 ] [ CH ] [ R4 ] [ G3 ] [ CC ] [ G2 ] [ G1 ] [ToJL] |
-                --------------------------------------------------------------------------------
-                */
+        //Middle Rows
+        System.out.println("| " + colorSpaceString("B3", "[ B3 ]") +
+                " -------------------------------------------------------------- " +
+                colorSpaceString("E1", "[ E1 ]") + " |");
+
+        System.out.println("| " + colorSpaceString("B2", "[ B2 ]") +
+                " |                                                            | " +
+                colorSpaceString("CH_RightCol", "[ CH ]") + " |");
+
+        System.out.println("| " + colorSpaceString("CH_LeftCol", "[ CH ]") +
+                " |                                                            | " +
+                colorSpaceString("E2", "[ E2 ]") + " |");
+
+        System.out.println("| " + colorSpaceString("B1", "[ B1 ]") +
+                " |                                                            | " +
+                colorSpaceString("E3", "[ E3 ]") + " |");
+
+        System.out.println("| " + colorSpaceString("R1", "[ R1 ]") +
+                " |                      M O N O P O L Y                       | " +
+                colorSpaceString("R3", "[ R3 ]") + " |");
+
+        System.out.println("| " + colorSpaceString("InTx", "[InTx]") +
+                " |                                                            | " +
+                colorSpaceString("F1", "[ F1 ]") + " |");
+
+        System.out.println("| " + colorSpaceString("A2", "[ A2 ]") +
+                " |                                                            | " +
+                colorSpaceString("F2", "[ F2 ]") + " |");
+
+        System.out.println("| " + colorSpaceString("CC_LeftCol", "[ CC ]") +
+                " |                                                            | " +
+                colorSpaceString("WW", "[ WW ]") + " |");
+
+        System.out.println("| " + colorSpaceString("A1", "[ A1 ]") +
+                " -------------------------------------------------------------- " +
+                colorSpaceString("F3", "[ F3 ]") + " |");
+
+        //TopRow
+        String bottomRowString = "| ";
+        bottomRowString += colorSpaceString("GO", "[ GO ]");
+        bottomRowString += " ";
+        bottomRowString += colorSpaceString("H2", "[ H2 ]");
+        bottomRowString += " ";
+        bottomRowString += colorSpaceString("LuTx", "[LuTx]");
+        bottomRowString += " ";
+        bottomRowString += colorSpaceString("H1", "[ H1 ]");
+        bottomRowString += " ";
+        bottomRowString += colorSpaceString("CH_bottomRow", "[ CH ]");
+        bottomRowString += " ";
+        bottomRowString += colorSpaceString("R4", "[ R4 ]");
+        bottomRowString += " ";
+        bottomRowString += colorSpaceString("G3", "[ G3 ]");
+        bottomRowString += " ";
+        bottomRowString += colorSpaceString("CC_bottomRow", "[ CC ]");
+        bottomRowString += " ";
+        bottomRowString += colorSpaceString("G2", "[ G2 ]");
+        bottomRowString += " ";
+        bottomRowString += colorSpaceString("G1", "[ G1 ]");
+        bottomRowString += " ";
+        bottomRowString += colorSpaceString("ToJL", "[ToJL]");
+        bottomRowString += " |";
+        System.out.println(bottomRowString);
+
+        System.out.println("--------------------------------------------------------------------------------");
     }
 
-    public void colorSpaceString(String spaceName, String uncoloredOutput) {
+    private String colorSpaceString(String spaceName, String uncoloredOutput) {
         String RESET = "\033[0m";
 
         String char0 = uncoloredOutput.substring(0, 1);
@@ -49,8 +121,9 @@ public class Game {
                 chars[i] = ("" + currentPlayer.data.getColorString() + chars[i] + RESET);
             }
             currentPlayer = currentPlayer.next;
+            i++;
         }
 
-        System.out.print("" + chars[0] + chars[1] + chars[2] + chars[3] + chars[4] + chars[5]);
+        return ("" + chars[0] + chars[1] + chars[2] + chars[3] + chars[4] + chars[5]);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
 
     public static void main(String[] args) {
-        CircularLinkedList<Integer> myLinkedList = new CircularLinkedList<Integer>();
-        myLinkedList.insertFirst(10);
-        myLinkedList.insertFirst(5);
-        myLinkedList.insertFirst(3);
-        myLinkedList.devPrint(10);
     }
 }
