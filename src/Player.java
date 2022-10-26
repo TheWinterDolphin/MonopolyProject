6c2e42cd2f19c62395d2e97442ecfda2cdd4c22c
TheWinterDolphin <andrew.sorak@gmail.com>
2022-10-25 21:01:02 -0500

    utility, taxes, jail,

--- src/Player.java
+++ src/Player.java
@@ -8,6 +8,7 @@ public class Player {
     private int money;
     private int turnsLeftInJail;
     private int numOfRailroadsOwned;
+    private int numOfUtilitiesOwned;
     private ArrayList<BoardSpace> properties;
     private int numGetOutOfJailFree;
 
@@ -96,5 +97,13 @@ public class Player {
 
     public void setForegroundColorString(String foregroundColorString) {
         this.foregroundColorString = foregroundColorString;
+    }
+
+    public int getNumOfUtilitiesOwned() {
+        return numOfUtilitiesOwned;
+    }
+
+    public void setNumOfUtilitiesOwned(int numOfUtilitiesOwned) {
+        this.numOfUtilitiesOwned = numOfUtilitiesOwned;
     }
 }
